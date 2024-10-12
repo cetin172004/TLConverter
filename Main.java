1d8cf175a3e2b8f8355f425f81595459556c8fb9
Ahmet Mustafa ÇETİN <cetin172004@gmail.com>
2024-10-13 02:24:57 +0300

    fonts

--- Main.java
+++ Main.java
@@ -6,45 +6,93 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         StackPane root = new StackPane();
-        
+        // sources
+        Font font1 = Font.loadFont(getClass().getResourceAsStream("fonts/Play-Regular.ttf"), 30);
+        Font font2 = Font.loadFont(getClass().getResourceAsStream("fonts/Play-Regular.ttf"), 40);
+        Font font3 = Font.loadFont(getClass().getResourceAsStream("fonts/Play-Regular.ttf"), 15);
+        //Image lira_image = new Image(getClass().getResourceAsStream("/images/myimage.png"));
         
         // ------------------------------------------------------------
         // TL Label
-        Label tl_label = new Label("37.495513₺");
-        tl_label.setStyle("-fx-font-size: 32px; -fx-text-fill: #146C94;");
+        Label tl_label = new Label("37.495513");
+        tl_label.setFont(font2);
         
         HBox tl_label_box = new HBox(tl_label);
         tl_label_box.setAlignment(javafx.geometry.Pos.CENTER);
         
         // ------------------------------------------------------------
         // Other Currencies
+		/* buttons */
 		Button usd_button = new Button("$");
 		Button eur_button = new Button("€");
 		Button gbp_button = new Button("£");
 		Button jpy_button = new Button("¥");
 
+		/* labels */
+		Label usd_label = new Label("USD");
+		usd_label.setFont(font3);
+		Label eur_label = new Label("EUR");
+		eur_label.setFont(font3);
+		Label gbp_label = new Label("GBP");
+		gbp_label.setFont(font3);
+		Label jpy_label = new Label("JPY");
+		jpy_label.setFont(font3);
+		
+		/* buttons styles */
+		usd_button.setShape(new Circle(50));
+		usd_button.setFont(font1);
+		eur_button.setShape(new Circle(50));
+		eur_button.setFont(font1);
+		gbp_button.setShape(new Circle(50));
+		gbp_button.setFont(font1);
+		jpy_button.setShape(new Circle(50));
+		jpy_button.setFont(font1);
+
+		/* vertical boxes */
+		VBox usd_box = new VBox(10);
+		usd_box.getChildren().addAll(usd_button, usd_label);
+		usd_box.setAlignment(javafx.geometry.Pos.CENTER);
+		
+		VBox eur_box = new VBox(10);
+		eur_box.getChildren().addAll(eur_button, eur_label);
+		eur_box.setAlignment(javafx.geometry.Pos.CENTER);
+		
+		VBox gbp_box = new VBox(10);
+		gbp_box.getChildren().addAll(gbp_button, gbp_label);
+		gbp_box.setAlignment(javafx.geometry.Pos.CENTER);
+		
+		VBox jpy_box = new VBox(10);
+		jpy_box.getChildren().addAll(jpy_button, jpy_label);
+		jpy_box.setAlignment(javafx.geometry.Pos.CENTER);
+	
+		/* currencies box */
         HBox currencies_box = new HBox(30);
-        currencies_box.getChildren().addAll(usd_button, eur_button, gbp_button, jpy_button);
+        currencies_box.getChildren().addAll(usd_box, eur_box, gbp_box, jpy_box);
         currencies_box.setAlignment(javafx.geometry.Pos.CENTER);
         
         // ------------------------------------------------------------
         
         // mainbox
-        VBox mainbox = new VBox(200);
+        VBox mainbox = new VBox(125);
         mainbox.getChildren().addAll(tl_label_box, currencies_box);
         mainbox.setAlignment(javafx.geometry.Pos.CENTER);
         
         // root Properties
         root.getChildren().addAll(mainbox);
-        root.setStyle("-fx-background-color: #F6F1F1;");
+        root.setStyle("-fx-background-color: white;");
         
-        Scene scene = new Scene(root,600, 500);
+        Scene scene = new Scene(root,400, 600);
         primaryStage.setTitle("Turkish Lira Converter");
+        primaryStage.getIcons().add(new Image("src/tlconverter-icon.png"));
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
